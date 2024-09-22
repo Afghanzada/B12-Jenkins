3faf2689d0ff1ddcecb4dff13ddba191e369d02f
hasib <hasib@10.0.0.138>
2024-09-21 19:34:05 -0700

    test

--- src/test/java/com/steps/LoginStep.java
+++ src/test/java/com/steps/LoginStep.java
@@ -22,10 +22,10 @@ public class LoginStep {
 	@Given("User navigates to login page {string}")
 	public void user_navigates_to_login_page(String url) throws InterruptedException {
 	 
-//		WebDriverManager.safaridriver().setup();
-//		driver = new SafariDriver();
-	 	WebDriverManager.edgedriver().setup();
-		 driver = new EdgeDriver();
+ 		WebDriverManager.firefoxdriver().setup();
+ 		driver = new FirefoxDriver();
+	 //	WebDriverManager.edgedriver().setup();
+	//	 driver = new EdgeDriver();
 		Thread.sleep(2000);
 		driver.manage().window().maximize();
 		driver.get(url);

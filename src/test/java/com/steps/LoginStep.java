b744954c22aef1800455ec22c7541b1f02a882e1
hasib <hasib@10.0.0.138>
2024-09-21 19:26:13 -0700

    abc

--- src/test/java/com/steps/LoginStep.java
+++ src/test/java/com/steps/LoginStep.java
@@ -1,4 +1,5 @@
 package com.steps;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -6,11 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import junit.framework.Assert;
+
 public class LoginStep {
 	
 	WebDriver driver;
@@ -18,35 +21,65 @@ public class LoginStep {
 	
 	@Given("User navigates to login page {string}")
 	public void user_navigates_to_login_page(String url) throws InterruptedException {
-		System.out.println("Navegating to appliciaton");
-	
-
+	 
+//		WebDriverManager.safaridriver().setup();
+//		driver = new SafariDriver();
+	 	WebDriverManager.edgedriver().setup();
+		 driver = new EdgeDriver();
+		Thread.sleep(2000);
+		driver.manage().window().maximize();
+		driver.get(url);
+		Thread.sleep(2000);
+		
+		
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
 	}
+
 	@When("User enters username {string}")
 	public void user_enters_username(String username) {
-		System.out.println("Entering username");
-		System.out.println("Hasib");
-		System.out.println("KH");
-	
+	  WebElement usernameField = driver.findElement(By.xpath("//input[@name='username']"));
+	  usernameField.sendKeys(username);
 	}
+
 	@When("User enters password {string}")
 	public void user_enters_password(String password) {
- 
-		System.out.println("Entering password");
+		  WebElement passwordField = driver.findElement(By.xpath("//input[@name='password']"));
+		  passwordField.sendKeys(password);
 	}
+
 	@When("User clicks login button")
 	public void user_clicks_login_button() {
-		System.out.println("clicking login button");
+		  WebElement loginButton = driver.findElement(By.xpath("//button[@type = 'submit']"));
+		  loginButton.click();
 	}
+
 	@Then("System redirects to dashboard page {string}")
 	public void system_redirects_to_dashboard_page(String pageHeader) throws InterruptedException {
-		System.out.println("pageheader text");
+	   System.out.println("Page Header... " +  pageHeader);
+	   
+	   System.out.println(driver.getTitle());
+	   
+	   Thread.sleep(2000);
+	   driver.quit();
 	}
+
+ 
 	
 	@Then("System redirects to dashboard pages {string}")
 	public void system_redirects_to_dashboard_pages(String string) {
-		System.out.println("dashboard page");
-		System.out.println("Done");
+		try {
+			System.out.println("aseetion failure...");
+			//Assert.assertEquals("java", "not java");
+			//Assert.fail();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	   
+		System.out.println("DONE...");
 	}
+ 
 }
+
 
